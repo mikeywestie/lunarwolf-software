90a5967e60ec074732bd59ec47113d875f217d9b
Mikey <mikeywestman@gmail.com>
2026-07-11 12:33:20 +0200

    feat(work): mount featured projects milestone

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import BreakRoomPortal from './BreakRoomPortal'
+import FeaturedProjectsPortal from './FeaturedProjectsPortal'
 import '@fontsource-variable/inter/standard.css'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <FeaturedProjectsPortal />
     <BreakRoomPortal />
   </StrictMode>,
 )
